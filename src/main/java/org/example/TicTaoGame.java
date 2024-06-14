504827f268c8c96bf667f4ab6628ad31e4ee69e8
Михаил <seliverstov8@yandex.ru>
2024-06-14 13:48:51 +0500

    Выявление ошибок в коде

--- src/main/java/org/example/TicTaoGame.java
+++ src/main/java/org/example/TicTaoGame.java
@@ -1,12 +1,9 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTaoGame {
-
-
-
-
 
 
     public static void main(String[] args) {
@@ -14,11 +11,14 @@ public class TicTaoGame {
         int[][] array = new int[3][3];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                    int x = scanner.nextInt();
-                    int y = scanner.nextInt();
-                    int sym = scanner.nextInt();
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                int sym = scanner.nextInt();
+                do {
                     array[x][y] = sym;
+                } while (x > 2 && y > 2);
             }
         }
+        System.out.println(Arrays.deepToString(array));
     }
 }
